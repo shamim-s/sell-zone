4e7319a25ec270e702d8d94214524c7851f8025e
Shamim Sarkar <shamimibas@gmail.com>
2022-11-26 14:42:07 +0600

    Added all users/buyers from database

--- src/Dashbord/AllUser/AllUser.js
+++ src/Dashbord/AllUser/AllUser.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
 
 const AllUser = () => {
-    return (
-        <div>
-            <h1>All Users</h1>
-        </div>
-    );
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/users/buyers`);
+      const data = await res.json();
+      return data;
+    },
+  });
+  return (
+    <div className="lg:mt-0 md:mt-4 mt-10">
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>
+   
+              </th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Action</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                users.map(user => <tr key={user._id}>
+                    <th>
+                      
+                    </th>
+                    <td>
+                      <div className="flex items-center space-x-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle w-12 h-12">
+                            <img
+                              src={user.img}
+                              alt="Avatar Tailwind CSS Component"
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <div className="font-bold">{user.name}</div>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      {user.email}
+                    </td>
+                    <td><button className="btn btn-xs rounded-sm bg-red-500 border-none">Delete</button></td>
+                  </tr>)
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllUser;
